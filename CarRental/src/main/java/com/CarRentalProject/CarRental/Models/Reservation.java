9a254e8a6099af5fe63d4af598f6aefb46ed056e
Redakadiri00 <reda.kdr.keni75@gmail.com>
2025-01-07 11:10:14 -0800

    pas important backend

--- CarRental/src/main/java/com/CarRentalProject/CarRental/Models/Reservation.java
+++ CarRental/src/main/java/com/CarRentalProject/CarRental/Models/Reservation.java
@@ -4,10 +4,8 @@ import jakarta.persistence.*;
 
 
 import java.time.LocalDate;
-import java.util.Date;
 
 import com.CarRentalProject.CarRental.Models.UserModels.User;
-import com.CarRentalProject.CarRental.Services.Status_reservation;
 
 @Entity
 @Table(name="reservation")
